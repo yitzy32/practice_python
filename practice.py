0c3ac52e23a5815264fe6537c08a916eec522a62
Yitzy Bratt <yitzybratt32@gmail.com>
2021-05-16 11:59:20 -0400

    concatenation and interpolation

--- practice.py
+++ practice.py
@@ -110,23 +110,82 @@
 
 # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
+# first_name = "Bob"
+# last_name = "Larsen"
+
+# print(first_name + " " + last_name)
+
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+# first_name = "Bob"
+# last_name = "Larsen"
+
+# print(f'{first_name} {last_name}')
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
+
+# word = input("Please type in a word ")
+# if word == "marco":
+#     print("polo")
+
 # 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+# first_color = "blue"
+# second_color = "green"
+# third_color = "beige"
+
+# print("The sky is so" + " " + first_color + " " + "and the grass is so " +
+#       second_color + " " + "and that building is " + third_color)
 
 # 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
 
+
+# first_color = "blue"
+# second_color = "green"
+# third_color = "beige"
+
+# print(
+#     f'The sky is so {first_color} and the grass is so {second_color} and that building is {third_color}')
+
 # 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+# name = input("Please enter a name ")
+
+# if name != "Santa":
+#     print("Your not Santa...")
 
 # 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
 
+# title = "From Head to Toe"
+# author = "Eric Carl"
+
+# print("The book " + title + " " + "by " + author + " " + "is a great read")
+
 # 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+# title = "From Head to Toe"
+# author = "Eric Carl"
+
+# print(f'The book {title} by {author} is a great read')
 
 # 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
 
+# password = input("Please enter a password ")
+
+# if password == "Joshua":
+#     print("Shall we play a game?")
+# else:
+#     print("access denied")
+
 # 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+# city_one = "New York"
+# city_two = "New Jersey"
+# city_three = "Connecticut"
+
+# print(city_one + " " + city_two + " " + "and " +
+#       city_three + " make up the tristate area")
 
 # SOLUTIONS: https://gist.github.com/peterxjang/79ba5acc912a86cce2a18c2f14b1c712
 
