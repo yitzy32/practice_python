2fb1139e772161fb3ff1c199906ae9953e915bcf
Yitzy Bratt <yitzybratt32@gmail.com>
2021-05-27 23:10:49 -0400

    delete extra file

--- practice.py
+++ practice.py
@@ -434,28 +434,6 @@
 
 # SOLUTIONS: https://gist.github.com/peterxjang/7095a2b19e1da2cc18d4a0dcd66cb8f1
 
-# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
-
-# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
-
-# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
-
-# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
-
-# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
-
-# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
-
-# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
-
-# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
-
-# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
-
-# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
-
-# SOLUTIONS: https://gist.github.com/peterxjang/7095a2b19e1da2cc18d4a0dcd66cb8f1
-
 # 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
 
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
